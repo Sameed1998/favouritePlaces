107f7072a50fba6c833d6ac0157f8e37c65078c3
Sameed Sharif <sameedsharif54@gmail.com>
2023-07-29 17:03:12 +0500

    changes

--- util/location.js
+++ util/location.js
@@ -1,5 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyADaEpiFSeltBH4uNI9aZaIM1XRXFfPvhs";
-// AIzaSyADaEpiFSeltBH4uNI9aZaIM1XRXFfPvhs mykey
+const GOOGLE_API_KEY = "Place Your Key Here."
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
